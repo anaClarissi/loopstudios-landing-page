d7f049b61a58e4a7e2fcfd0f19268dfb0c8c0597
Ana Clarissi <anaclarissicruz@gmail.com>
2026-06-23 17:39:24 -0300

    feat: main section desktop design

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,44 @@
-const imagesName = ["deep-earth", "night-arcade", "soccer-team", "grid", "from-above", "pocket-borealis", "curiosity", "fisheye"];
+function showImages() {
 
-const cards = document.querySelectorAll(".creations-grid .card");
+    const imagesName = ["deep-earth", "night-arcade", "soccer-team", "grid", "from-above", "pocket-borealis", "curiosity", "fisheye"];
 
-cards.forEach((card, index) => {
+    const cards = document.querySelectorAll(".creations-grid .card");
 
-    card.style.backgroundImage = `url('./src/images/mobile/image-${imagesName[index]}.jpg')`;
+    const windowWidth = window.innerWidth;
 
-});
+    const imageSrc = windowWidth < 960 ? "mobile" : "desktop";
+
+    cards.forEach((card, index) => {
+
+        card.style.backgroundImage = `url('./src/images/${imageSrc}/image-${imagesName[index]}.jpg')`;
+
+    });
+}
+
+function checkBtnSeeAll() {
+
+    const windowWidth = window.innerWidth;
+
+    const btnSeeAllDesktop = document.querySelector(".btn-see-all.desktop");
+
+    const btnSeeAllMobile = document.querySelector(".btn-see-all.mobile");
+
+    if (windowWidth < 960) {
+
+        btnSeeAllDesktop.hidden = true;
+
+        btnSeeAllMobile.hidden = false;
+
+    } else {
+
+        btnSeeAllDesktop.hidden = false;
+
+        btnSeeAllMobile.hidden = true;
+
+    }
+
+}
+
+checkBtnSeeAll();
+
+showImages();
